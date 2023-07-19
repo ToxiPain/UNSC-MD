f87bea5b2f8dc7763a7936d2eb9b2bd69b26668c
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2023-07-19 10:44:39 -0600

    🥀𝐔𝐍𝐒𝐂-𝐁𝐨𝐭🥀

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*')
+m.reply('*┊↳🎖️┊ 𝐄𝐥 𝐛𝐨𝐭 𝐡𝐚 𝐬𝐢𝐝𝐨 𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨 𝐜𝐨𝐧 𝐞𝐱𝐢𝐭𝐨 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐜𝐡𝐚𝐭!*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = /^unbanchat$/i
+handler.command = /^on$/i
 handler.rowner = true
 export default handler
