2670d8d17dfb55371f2f6c4020a18a8c77d391f2
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2023-07-19 09:44:57 -0600

    🥀𝐔𝐍𝐒𝐂-𝐁𝐨𝐭🥀

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TI
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`*┊↳🎖️┊ 𝐇𝐨𝐥𝐚 @${m.sender.split`@`[0]}, 𝐄𝐬𝐭á 𝐩𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐨 𝐡𝐚𝐛𝐥𝐚𝐫 𝐚𝐥 𝐩𝐫𝐢𝐯𝐚𝐝𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭, 𝐩𝐨𝐫 𝐥𝐨 𝐭𝐚𝐧𝐭𝐨 𝐬𝐞𝐫á𝐬 𝐛𝐥𝐨𝐪𝐮𝐞𝐚𝐝𝐨.*`, false, { mentions: [m.sender] })
+await m.reply(`*┊↳🎖️┊ 𝐇𝐨𝐥𝐚 @${m.sender.split`@`[0]}, 𝐄𝐬𝐭á 𝐩𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐨 𝐡𝐚𝐛𝐥𝐚𝐫 𝐚𝐥 𝐩𝐫𝐢𝐯𝐚𝐝𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭, 𝐩𝐨𝐫 𝐥𝐨 𝐭𝐚𝐧𝐭𝐨 𝐬𝐞𝐫á𝐬 𝐛𝐥𝐨𝐪𝐮𝐞𝐚𝐝𝐨...*`, false, { mentions: [m.sender] })
 await this.updateBlockStatus(m.chat, 'block')}
 return !1
 }
